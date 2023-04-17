e7ed0666279f1c0cfcc0c7e25c5444a112301d20
phamlong312 <longpdph17654@fpt.edu.vn>
2023-04-17 15:59:10 +0700

    UI Login and Register

--- app/src/main/java/com/example/findaroomver2/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/findaroomver2/ui/activity/LoginActivity.java
@@ -2,15 +2,49 @@ package com.example.findaroomver2.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.findaroomver2.MainActivity;
 import com.example.findaroomver2.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputLayout txtUsername, txtPassword;
+    Button btnSignIn;
+    TextView tvDangKy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+
+        btnSignIn = findViewById(R.id.btn_Login);
+        tvDangKy = findViewById(R.id.tv_SignUp);
+
+        final  Intent intentMain = new Intent(this, MainActivity.class);
+        final  Intent intentRegister = new Intent(this, RegisterActivity.class);
+
+        //ấn đăng nhập chuyển vào màn hình chính
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(intentMain);
+            }
+        });
+
+        //ấn vào màn hình đăng ký tài khoản
+        tvDangKy.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(intentRegister);
+            }
+        });
+   }
 }
--- app/src/main/java/com/example/findaroomver2/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/example/findaroomver2/ui/activity/RegisterActivity.java
@@ -2,15 +2,35 @@ package com.example.findaroomver2.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.findaroomver2.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btnDangKy;
+    TextView tvDangNhap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnDangKy = findViewById(R.id.btn_SignUp);
+        tvDangNhap = findViewById(R.id.tv_SignIn);
+
+        final Intent intentDangNhap = new Intent(this, LoginActivity.class);
+
+        tvDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentDangNhap);
+            }
+        });
+
     }
 }
