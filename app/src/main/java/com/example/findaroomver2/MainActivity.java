61adf75b6d3a86269d1a636d95111522829d6702
phamlong312 <longpdph17654@fpt.edu.vn>
2023-04-21 09:23:43 +0700

    UI Bottomnavigtion

--- app/src/main/java/com/example/findaroomver2/MainActivity.java
+++ app/src/main/java/com/example/findaroomver2/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.findaroomver2;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.findaroomver2.databinding.ActivityMainBinding;
+import com.example.findaroomver2.ui.adapter.FragmentViewPagerAdapter;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    public ActivityMainBinding activityMainBinding;
+    private FragmentViewPagerAdapter viewPagerAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        viewPagerAdapter = new FragmentViewPagerAdapter(getSupportFragmentManager(), FragmentViewPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        activityMainBinding.vpHome.setAdapter(viewPagerAdapter);
+        activityMainBinding.vpHome.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(
+                    int position,
+                    float positionOffset,
+                    int positionOffsetPixels
+            ) {
+
+            }
+            @Override
+            public void onPageSelected(int position) {
+                activityMainBinding.bottomBar.selectTabAt(position, true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        activityMainBinding.bottomBar.setupWithViewPager(activityMainBinding.vpHome);
+        activityMainBinding.bottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(
+                    int i,
+                    AnimatedBottomBar.Tab tab,
+                    int i1,
+                    @NonNull AnimatedBottomBar.Tab tab1
+            ) {
+                activityMainBinding.vpHome.setCurrentItem(i1);
+            }
+
+            @Override
+            public void onTabReselected(int i, @NonNull AnimatedBottomBar.Tab tab) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/findaroomver2/ui/adapter/FragmentViewPagerAdapter.java
@@ -0,0 +1,70 @@
+package com.example.findaroomver2.ui.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.findaroomver2.ui.fragment.HomeFragment;
+import com.example.findaroomver2.ui.fragment.NotificationFragment;
+import com.example.findaroomver2.ui.fragment.PostFragment;
+import com.example.findaroomver2.ui.fragment.PostNewsFragment;
+import com.example.findaroomver2.ui.fragment.UserFragment;
+
+public class FragmentViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    HomeFragment homeFragment;
+    NotificationFragment notificationFragment;
+    PostNewsFragment postNewsFragment;
+    PostFragment postFragment;
+    UserFragment userFragment;
+    private Fragment mCurrentFragment;
+
+    public FragmentViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        postNewsFragment = new PostNewsFragment();
+        postFragment = new PostFragment();
+        userFragment = new UserFragment();
+    }
+
+    public Fragment getCurrentFragment() {
+        return mCurrentFragment;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 1:
+                return notificationFragment;
+            case 2:
+                return postNewsFragment;
+            case 3:
+                return postFragment;
+            case 4:
+                return userFragment;
+            default:
+                return homeFragment;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+        if (getCurrentFragment() != object) {
+            mCurrentFragment = ((Fragment) object);
+        }
+        super.setPrimaryItem(container, position, object);
+    }
+
+
+}
+
