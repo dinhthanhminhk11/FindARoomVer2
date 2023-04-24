b4615f0cad453df480ae92a4ed364c6084a76004
phamlong312 <longpdph17654@fpt.edu.vn>
2023-04-24 16:08:17 +0700

    list post ui, tablayout , view pager

---
+++ app/src/main/java/com/example/findaroomver2/ui/activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.findaroomver2.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.findaroomver2.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/example/findaroomver2/ui/adapter/PostViewPagerAdapter.java
@@ -0,0 +1,88 @@
+package com.example.findaroomver2.ui.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.findaroomver2.ui.fragment.BestPriceFragment;
+import com.example.findaroomver2.ui.fragment.NearestPosrFragment;
+import com.example.findaroomver2.ui.fragment.NewestPostFragment;
+import com.example.findaroomver2.ui.fragment.WatchedFragment;
+
+public class PostViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    NewestPostFragment newestPostFragment;
+    NearestPosrFragment nearestPosrFragment;
+    BestPriceFragment bestPriceFragment;
+    WatchedFragment watchedFragment;
+
+    private Fragment mPostCurrentFragment;
+
+    public PostViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+
+        newestPostFragment = new NewestPostFragment();
+        nearestPosrFragment = new NearestPosrFragment();
+        bestPriceFragment = new BestPriceFragment();
+        watchedFragment = new WatchedFragment();
+    }
+
+//    public Fragment getmPostCurrentFragment(){
+//        return mPostCurrentFragment;
+//    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return newestPostFragment;
+            case 1:
+                return nearestPosrFragment;
+            case 2:
+                return bestPriceFragment;
+            case 3:
+                return watchedFragment;
+            default:
+                return newestPostFragment;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position){
+            case 0:
+                title = "Bài viết mới nhất";
+                break;
+            case 1:
+                title = "Gần bạn nhất";
+                break;
+            case 2:
+                title = "Giá tốt nhất";
+                break;
+            case 3:
+                title = "Xem gần đây";
+                break;
+        }
+        return title;
+    }
+
+//    @Override
+//    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+//        if (getmPostCurrentFragment() != object) {
+//            mPostCurrentFragment = ((Fragment) object);
+//        }
+//        super.setPrimaryItem(container, position, object);
+//    }
+}
