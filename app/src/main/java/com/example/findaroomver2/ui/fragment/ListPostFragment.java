ba762176f4a3dc9358f808b069948f958f8e066d
phamlong312 <longpdph17654@fpt.edu.vn>
2023-04-25 14:06:51 +0700

    thêm dialog xác nhận, thêm giao diện thay đổi thông tin tài khoản

--- app/src/main/java/com/example/findaroomver2/ui/fragment/ListPostFragment.java
+++ app/src/main/java/com/example/findaroomver2/ui/fragment/ListPostFragment.java
@@ -2,6 +2,8 @@ package com.example.findaroomver2.ui.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -24,19 +26,24 @@ public class ListPostFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         fragmentListPostBinding = FragmentListPostBinding.inflate(getLayoutInflater());
-        postViewPagerAdapter = new PostViewPagerAdapter(getChildFragmentManager(), PostViewPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
 
-        fragmentListPostBinding.postViewPager.setAdapter(postViewPagerAdapter);
-
-        fragmentListPostBinding.postTablayout.setupWithViewPager(fragmentListPostBinding.postViewPager);
+//        postViewPagerAdapter = new PostViewPagerAdapter(getChildFragmentManager(), PostViewPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+//        fragmentListPostBinding.postViewPager.setAdapter(postViewPagerAdapter);
+//        fragmentListPostBinding.postTablayout.setupWithViewPager(fragmentListPostBinding.postViewPager);
 
         return fragmentListPostBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        postViewPagerAdapter = new PostViewPagerAdapter(getChildFragmentManager(), PostViewPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragmentListPostBinding.postViewPager.setAdapter(postViewPagerAdapter);
+        fragmentListPostBinding.postTablayout.setupWithViewPager(fragmentListPostBinding.postViewPager);
     }
 }
