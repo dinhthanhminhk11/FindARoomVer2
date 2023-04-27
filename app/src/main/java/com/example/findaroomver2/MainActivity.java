dc2672380a389e3009a5cacc40604a1d6a4cf60a
hoangdung <dunghaph13370@fpt.edu.vn>
2023-04-28 00:33:25 +0700

    change bottom navigation

--- app/src/main/java/com/example/findaroomver2/MainActivity.java
+++ app/src/main/java/com/example/findaroomver2/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.findaroomver2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
@@ -19,16 +22,19 @@ import com.example.findaroomver2.model.UserClient;
 import com.example.findaroomver2.response.UserResponseLogin;
 import com.example.findaroomver2.sharedpreferences.MySharedPreferences;
 import com.example.findaroomver2.ui.adapter.FragmentViewPagerAdapter;
+import com.example.findaroomver2.ui.fragment.HomeFragment;
+import com.example.findaroomver2.ui.fragment.ListPostFragment;
+import com.example.findaroomver2.ui.fragment.NotificationFragment;
+import com.example.findaroomver2.ui.fragment.PostFragment;
+import com.example.findaroomver2.ui.fragment.UserFragment;
 import com.example.findaroomver2.viewmodel.MainViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import nl.joery.animatedbottombar.AnimatedBottomBar;
-
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private int backPressedCount = 0;
     private final int MAX_BACK_PRESS_COUNT = 2;
     private final int BACK_PRESS_TIME_INTERVAL = 2000;
@@ -45,44 +51,14 @@ public class MainActivity extends AppCompatActivity {
 
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
+        activityMainBinding.bottomBar.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+
         String token = MySharedPreferences.getInstance(this).getString(AppConstant.USER_TOKEN, "");
 
         if (token != null || !token.equals("")) {
             mainViewModel.loginToken(token);
         }
-
-        viewPagerAdapter = new FragmentViewPagerAdapter(getSupportFragmentManager(), FragmentViewPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-
-        activityMainBinding.vpHome.setAdapter(viewPagerAdapter);
-        activityMainBinding.vpHome.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                activityMainBinding.bottomBar.selectTabAt(position, true);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        activityMainBinding.bottomBar.setupWithViewPager(activityMainBinding.vpHome);
-        activityMainBinding.bottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
-            @Override
-            public void onTabSelected(int i, AnimatedBottomBar.Tab tab, int i1, AnimatedBottomBar.Tab tab1) {
-                activityMainBinding.vpHome.setCurrentItem(i1);
-            }
-
-            @Override
-            public void onTabReselected(int i, @NonNull AnimatedBottomBar.Tab tab) {
-
-            }
-        });
 
         backPressToast = Toast.makeText(getApplicationContext(), "Nhấn lần nữa để thoát", Toast.LENGTH_SHORT);
 
@@ -131,7 +107,31 @@ public class MainActivity extends AppCompatActivity {
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     public void onMessageEvent(KeyEvent event) {
         if (event.getIdEven() == NotificationCenter.create_post_success) {
-            activityMainBinding.vpHome.setCurrentItem(3);
+
         }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.tab_home) {
+            loadFragment(new HomeFragment());
+            return true;
+        } else if (id == R.id.tab_notification) {
+            loadFragment(new NotificationFragment());
+            return true;
+        } else if (id == R.id.tab_upload) {
+            loadFragment(new PostFragment());
+            return true;
+        }  else if (id == R.id.tab_user) {
+            loadFragment(new UserFragment());
+            return true;
+        }
+        return false;
+    }
+
+    void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
     }
 }
